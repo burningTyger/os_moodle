df280de05bc6e1f07556b6317f54dd9eef32d6ad
burningTyger <b6tyger@gmail.com>
2012-03-03 11:03:32 +0100

    weekly moodle 2.1.4+ update

--- php/lib/weblib.php
+++ php/lib/weblib.php
@@ -494,7 +494,13 @@ class moodle_url {
             $params = $this->params;
         }
         foreach ($params as $key => $val) {
-           $arr[] = rawurlencode($key)."=".rawurlencode($val);
+            if (is_array($val)) {
+                foreach ($val as $index => $value) {
+                    $arr[] = rawurlencode($key.'['.$index.']')."=".rawurlencode($value);
+                }
+            } else {
+                $arr[] = rawurlencode($key)."=".rawurlencode($val);
+            }
         }
         if ($escaped) {
             return implode('&amp;', $arr);
@@ -2843,7 +2849,7 @@ function debugging($message = '', $level = DEBUG_NORMAL, $backtrace = null) {
     global $CFG, $USER, $UNITTEST;
 
     $forcedebug = false;
-    if (!empty($CFG->debugusers)) {
+    if (!empty($CFG->debugusers) && $USER) {
         $debugusers = explode(',', $CFG->debugusers);
         $forcedebug = in_array($USER->id, $debugusers);
     }
